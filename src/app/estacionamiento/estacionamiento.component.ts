492c55425954ff8f61212d3043d3bba2c70ddfd6
LucianaMendozaT <lucianamend0124@gmail.com>
2023-11-25 20:05:21 -0500

    segundo  commit

--- src/app/estacionamiento/estacionamiento.component.ts
+++ src/app/estacionamiento/estacionamiento.component.ts
@@ -3,8 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-estacionamiento',
   templateUrl: './estacionamiento.component.html',
-  styleUrl: './estacionamiento.component.css'
+  styleUrls: ['./estacionamiento.component.css']
 })
 export class EstacionamientoComponent {
+  hours: number = 0;
+  minutes: number = 0;
+  totalCost: number | null = null;
 
+  calcularCosto() {
+    const tiempoTotalEnHoras = this.hours + this.minutes / 60;
+    const costoPorHora = 1500; 
+
+    this.totalCost = tiempoTotalEnHoras * costoPorHora;
+  }
 }
